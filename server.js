789d58f129105e8b334d890af3c5f0b882299519
Treast <vincent.riva@outlook.fr>
2020-04-26 11:42:22 +0200

    Add migrations, Sequelize connections & strategies

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const passport = require('passport');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(passport.initialize());
+
+// Authentication strategies
+require('./strategies/login');
+require('./strategies/register');
+require('./strategies/jwt');
 
 app.get('/', (req, res) => {
   res.json({ message: 'Hello World !' });
