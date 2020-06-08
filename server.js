911c21c9895496f29c243b5bea79adbae1edb981
Vincent Riva <vincent.riva@outlook.fr>
2020-06-08 14:00:47 +0200

    Fix Sentry

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ app.use(bodyParser.json())
 app.use(passport.initialize())
 app.use(Sentry.Handlers.errorHandler())
 
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500
+  res.end(res.sentry + '\n')
+})
+
 // Authentication strategies
 require('./strategies/login')
 require('./strategies/register')
