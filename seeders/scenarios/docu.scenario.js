64aadd15ad3fbb40e84d577db603fce1ab687192
Elodie <elodie.oudot@gmail.com>
2020-06-09 21:45:16 +0200

    Fix scenario

---
+++ seeders/scenarios/docu.scenario.js
@@ -0,0 +1,83 @@
+module.exports = {
+    scenario: {
+      "uuid": "f8e6adab-7bd2-4925-b769-a154e011df67",
+      "name": "Le documentaire"
+    },
+    sentences: [
+      {
+        "uuid": "7ca10520-862c-43e4-a306-1d3635530947",
+        "interaction": false,
+        "order": 0,
+        "scenarioUuid": "f8e6adab-7bd2-4925-b769-a154e011df67"
+      },
+      {
+        "uuid": "ac555270-8442-4de4-9d5b-9839347cb600",
+        "interaction": false,
+        "order": 1,
+        "scenarioUuid": "f8e6adab-7bd2-4925-b769-a154e011df67"
+      },
+      {
+        "uuid": "896f746e-7211-4ebe-a090-a42c6e0b71c7",
+        "interaction": false,
+        "order": 2,
+        "scenarioUuid": "f8e6adab-7bd2-4925-b769-a154e011df67"
+      }
+    ],
+    oos: [
+      {
+        "scenarioUuid": "f8e6adab-7bd2-4925-b769-a154e011df67",
+        "ooUuid": "23c7e653-f1a6-435c-9f58-24f9efb8c5e1"
+      },
+      {
+        "scenarioUuid": "f8e6adab-7bd2-4925-b769-a154e011df67",
+        "ooUuid": "a10058d0-46e0-4e54-bf54-72bef1ae283f"
+      }
+    ],
+    audios: [
+      {
+        "uuid": "0e8ab7cb-f09d-4b3e-a24e-772b2b4d50c1",
+        "name": "ledocu-0-entree-negative-cinoche",
+        "url": "/scenario-docu/ledocu-0-entree-negative-cinoche.wav",
+        "type": "entry",
+        "audibleUuid": "f8e6adab-7bd2-4925-b769-a154e011df67",
+        "audibleType": "scenario",
+        "ooUuid": "23c7e653-f1a6-435c-9f58-24f9efb8c5e1"
+      },
+      {
+        "uuid": "5b542d47-68bf-4d7e-8671-3100762f3acd",
+        "name": "ledocu-0-entree-neutre-cinoche",
+        "url": "/scenario-docu/ledocu-0-entree-neutre-cinoche.wav",
+        "type": "entry",
+        "audibleUuid": "f8e6adab-7bd2-4925-b769-a154e011df67",
+        "audibleType": "scenario",
+        "ooUuid": "23c7e653-f1a6-435c-9f58-24f9efb8c5e1"
+      },
+      {
+        "uuid": "8b6ad1d8-e8e6-4bc2-b3df-2d4e8aa3fd26",
+        "name": "ledocu-1-yoga",
+        "url": "/scenario-docu/ledocu-1-yoga.wav",
+        "type": null,
+        "audibleUuid": "7ca10520-862c-43e4-a306-1d3635530947",
+        "audibleType": "sentence",
+        "ooUuid": "a10058d0-46e0-4e54-bf54-72bef1ae283f"
+      },
+      {
+        "uuid": "80fe443c-b69a-466b-9746-d198d18d143a",
+        "name": "ledocu-2-cinoche",
+        "url": "/scenario-docu/ledocu-2-cinoche.wev",
+        "type": null,
+        "audibleUuid": "ac555270-8442-4de4-9d5b-9839347cb600",
+        "audibleType": "sentence",
+        "ooUuid": "23c7e653-f1a6-435c-9f58-24f9efb8c5e1"
+      },
+      {
+        "uuid": "d7ce3368-5b81-4be3-8341-f1c52478e652",
+        "name": "ledocu-3-yoga",
+        "url": "/scenario-docu/ledocu-3-yoga.wav",
+        "type": null,
+        "audibleUuid": "896f746e-7211-4ebe-a090-a42c6e0b71c7",
+        "audibleType": "sentence",
+        "ooUuid": "a10058d0-46e0-4e54-bf54-72bef1ae283f"
+      }
+    ]
+}
