91bc1c69f98ef670aa53b3515d7aebc934dc2178
Vincent Riva <vincent.riva@outlook.fr>
2020-06-10 11:18:33 +0200

    Update docu.scenario.js

--- seeders/scenarios/docu.scenario.js
+++ seeders/scenarios/docu.scenario.js
@@ -6,18 +6,21 @@ module.exports = {
   sentences: [
     {
       uuid: '7ca10520-862c-43e4-a306-1d3635530947',
+      name: 'ledocu-1-yoga',
       interaction: false,
       order: 0,
       scenarioUuid: 'f8e6adab-7bd2-4925-b769-a154e011df67',
     },
     {
       uuid: 'ac555270-8442-4de4-9d5b-9839347cb600',
+      name: 'ledocu-2-cinoche',
       interaction: false,
       order: 1,
       scenarioUuid: 'f8e6adab-7bd2-4925-b769-a154e011df67',
     },
     {
       uuid: '896f746e-7211-4ebe-a090-a42c6e0b71c7',
+      name: 'ledocu-3-yoga',
       interaction: false,
       order: 2,
       scenarioUuid: 'f8e6adab-7bd2-4925-b769-a154e011df67',
@@ -35,16 +38,16 @@ module.exports = {
   ],
   audios: [
     {
-      uuid: '0e8ab7cb-f09d-4b3e-a24e-772b2b4d50c1',
+      uuid: 'bff98087-bc3b-4b77-b0fe-d44ec249d11c',
       name: 'ledocu-0-entree-negative-cinoche',
       url: 'scenario-docu/ledocu-0-entree-negative-cinoche.wav',
-      type: 'entry',
+      type: 'entry:negative',
       audibleUuid: 'f8e6adab-7bd2-4925-b769-a154e011df67',
       audibleType: 'scenario',
       ooUuid: '23c7e653-f1a6-435c-9f58-24f9efb8c5e1',
     },
     {
-      uuid: '5b542d47-68bf-4d7e-8671-3100762f3acd',
+      uuid: '49d23202-1a6a-4f86-8c82-f2de887a6ebb',
       name: 'ledocu-0-entree-neutre-cinoche',
       url: 'scenario-docu/ledocu-0-entree-neutre-cinoche.wav',
       type: 'entry',
@@ -53,7 +56,7 @@ module.exports = {
       ooUuid: '23c7e653-f1a6-435c-9f58-24f9efb8c5e1',
     },
     {
-      uuid: '8b6ad1d8-e8e6-4bc2-b3df-2d4e8aa3fd26',
+      uuid: '391ae494-9fcf-48cb-b43a-f3371f7c77f2',
       name: 'ledocu-1-yoga',
       url: 'scenario-docu/ledocu-1-yoga.wav',
       type: null,
@@ -62,7 +65,7 @@ module.exports = {
       ooUuid: 'a10058d0-46e0-4e54-bf54-72bef1ae283f',
     },
     {
-      uuid: '80fe443c-b69a-466b-9746-d198d18d143a',
+      uuid: 'fe06463e-eaf8-4f16-bb9e-83196c2bfc37',
       name: 'ledocu-2-cinoche',
       url: 'scenario-docu/ledocu-2-cinoche.wav',
       type: null,
@@ -71,7 +74,7 @@ module.exports = {
       ooUuid: '23c7e653-f1a6-435c-9f58-24f9efb8c5e1',
     },
     {
-      uuid: 'd7ce3368-5b81-4be3-8341-f1c52478e652',
+      uuid: 'cd8c3713-6b66-4e06-a317-f83b3d9d082c',
       name: 'ledocu-3-yoga',
       url: 'scenario-docu/ledocu-3-yoga.wav',
       type: null,
