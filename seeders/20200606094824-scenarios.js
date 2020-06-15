1e4ab081551c6f0ae4393a5c95aae16262662af4
Vincent Riva <vincent.riva@outlook.fr>
2020-06-15 15:50:49 +0200

    Add scenarios

--- seeders/20200606094824-scenarios.js
+++ seeders/20200606094824-scenarios.js
@@ -2,12 +2,30 @@
 
 const { v4: uuidv4 } = require('uuid')
 
-const scenarioTest = require('./scenarios/test.scenario')
+// const scenarioTest = require('./scenarios/test.scenario')
+const scenarioBatman = require('./scenarios/batman.scenario')
+const scenarioClooney = require('./scenarios/clooney.scenario')
 const scenarioDocu = require('./scenarios/docu.scenario')
-const scenarioTisane = require('./scenarios/tisane.scenario')
+const scenarioPepito = require('./scenarios/pepito.scenario')
 const scenarioPlante = require('./scenarios/plante.scenario')
+const scenarioSeigneur = require('./scenarios/seigneur.scenario')
+const scenarioSituation = require('./scenarios/situation.scenario')
+const scenarioSpeed = require('./scenarios/speed.scenario')
+const scenarioTisane = require('./scenarios/tisane.scenario')
+const scenarioTractopelle = require('./scenarios/tractopelle.scenario')
 
-const scenariosJson = [scenarioTest, scenarioDocu, scenarioTisane, scenarioPlante]
+const scenariosJson = [
+  scenarioBatman,
+  scenarioClooney,
+  scenarioDocu,
+  scenarioPepito,
+  scenarioPlante,
+  scenarioSeigneur,
+  scenarioSituation,
+  scenarioSpeed,
+  scenarioTisane,
+  scenarioTractopelle,
+]
 
 const scenarios = []
 const scenariosOos = []
